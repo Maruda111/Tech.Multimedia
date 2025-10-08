42c1a64031fc539da361ae59d67febcaecbb5dfc
Szymon Tarasewicz <71249310+Maruda111@users.noreply.github.com>
2025-10-08 17:43:14 +0200

    Add missing links for zadanie2 in index.php

--- maruda111.pl/index.php
+++ maruda111.pl/index.php
@@ -5,9 +5,10 @@
 <body>
 
     <a href="/z1">zadanie1</a><br>
-    <a href="z2"></a>
+    <a href="z2">zadanie2</a>
     <a href="z3"></a>
     <a href="z4"></a>
     <a href="z5"></a>
     <a href="z6"></a>
+
 </body>
